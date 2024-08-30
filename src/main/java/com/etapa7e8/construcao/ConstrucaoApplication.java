e3329ed3bc868e240201c1dedaac843253b1c6c8
Paulo Rodrigo <roodrigobatista@hotmail.com>
2024-08-30 19:08:28 -0300

    Criação do projeto

---
+++ src/main/java/com/etapa7e8/construcao/ConstrucaoApplication.java
@@ -0,0 +1,13 @@
+package com.etapa7e8.construcao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstrucaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstrucaoApplication.class, args);
+	}
+
+}
