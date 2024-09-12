93face2dba42a5819041aee31fbdaf3c5310199c
Paulo Rodrigo <roodrigobatista@hotmail.com>
2024-09-12 20:20:25 -0300

    Criando entidade Login

---
+++ src/main/java/com/etapa7e8/construcao/model/Login.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.etapa7e8.construcao.model;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class Login {
+    
+}
