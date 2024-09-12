e2e6eeea4f12632e99c57cfc0ddf3ad7757c5fbc
Paulo Rodrigo <roodrigobatista@hotmail.com>
2024-09-12 20:26:51 -0300

    Entidade Login ja estruturado

--- src/main/java/com/etapa7e8/construcao/model/Login.java
+++ src/main/java/com/etapa7e8/construcao/model/Login.java
@@ -1,13 +1,59 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.etapa7e8.construcao.model;
 
-/**
- *
- * @author Rodrigo
- */
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "login")
 public class Login {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String usuario;
+    private String senha;
+    private String tipoDeUsuario;
+
+    public Login() {}
+
+    public Login(int id, String usuario, String senha, String tipoDeUsuario) {
+        this.id = id;
+        this.usuario = usuario;
+        this.senha = senha;
+        this.tipoDeUsuario = tipoDeUsuario;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTipoDeUsuario() {
+        return tipoDeUsuario;
+    }
+
+    public void setTipoDeUsuario(String tipoDeUsuario) {
+        this.tipoDeUsuario = tipoDeUsuario;
+    }
 }
