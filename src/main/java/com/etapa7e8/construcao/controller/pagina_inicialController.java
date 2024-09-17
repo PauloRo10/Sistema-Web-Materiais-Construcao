cfb8d6da7e011230125116557f19c338fa4db0d4
Paulo Rodrigo <roodrigobatista@hotmail.com>
2024-09-17 20:17:28 -0300

    Adicionando paginaCliente com CRUD e arquivo CSS

---
+++ src/main/java/com/etapa7e8/construcao/controller/pagina_inicialController.java
@@ -0,0 +1,38 @@
+
+package com.etapa7e8.construcao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class pagina_inicialController {
+    /*@GetMapping("/pagina_inicial")
+    public String mostrarPaginaInicial() {
+        return "pagina_inicial"; // Nome do arquivo HTML da página inicial
+    }*/
+
+    @GetMapping("/cliente")
+    public String mostrarPaginaCliente() {
+        return "paginaCliente"; // Nome do arquivo HTML da página de cliente
+    }
+/*
+    @GetMapping("/orcamento")
+    public String mostrarPaginaOrcamento() {
+        return "pagina_orcamento"; // Nome do arquivo HTML da página de orçamento
+    }
+
+    @GetMapping("/estoque")
+    public String mostrarPaginaEstoque() {
+        return "pagina_estoque"; // Nome do arquivo HTML da página de estoque
+    }
+
+    @GetMapping("/funcionario")
+    public String mostrarPaginaFuncionario() {
+        return "pagina_funcionario"; // Nome do arquivo HTML da página de funcionário
+    }
+
+    @GetMapping("/venda")
+    public String mostrarPaginaVenda() {
+        return "pagina_venda"; // Nome do arquivo HTML da página de venda
+    }*/
+}
